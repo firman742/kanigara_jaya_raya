085d79ba91ab0556511ee9717600c7275eb316ab
firman742 <hafidzfirman742@gmail.com>
2025-01-19 21:11:35 +0700

    pasang template dan membuat halaman layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +9,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $admin = Auth::user();
+    return view('Dashboard.index', compact('admin'));    // return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
