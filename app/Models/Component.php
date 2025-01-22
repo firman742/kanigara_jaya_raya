0dfa96a5cdc148e27508a1ee336e7de1fa79e1f1
firman742 <hafidzfirman742@gmail.com>
2025-01-22 13:15:16 +0700

    Create Seeder for data Component

--- app/Models/Component.php
+++ app/Models/Component.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Component extends Model
 {
     /** @use HasFactory<\Database\Factories\ComponentFactory> */
-    use HasFactory, SoftDeletes;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $table = 'components';
     protected $fillable = [
--- database/seeders/ComponentSeeder.php
+++ database/seeders/ComponentSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Component;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class ComponentSeeder extends Seeder
 {
@@ -12,6 +13,47 @@ class ComponentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $components = [
+            'Starter / Ignition System',
+            'Label Oli / Kartu OK',
+            'Radio / Tape Merk',
+            'STNK',
+            'KIR',
+            'Alarm',
+            'Lighter / Pemantik',
+            'Asbak',
+            'Seat Cover / Sarung Jok',
+            'Safety Belt',
+            'Head Rest',
+            'Lampu Plafond',
+            'Karpet Dasar',
+            'Dongkrak & Gagang',
+            'Kotak P3K',
+            'Segitiga Pengaman',
+            'Kunci Roda',
+            'Tool set',
+            'Kaca Spion Dalam',
+            'Kaca Spion Luar',
+            'Dop Roda 4 buah',
+            'Velg Racing',
+            'Karpet Bagasi',
+            'penutup ban',
+            'ban serep',
+            'antenna',
+            'cd player merk',
+            'Emblem / sticker',
+            'Lampu-lampu sen',
+            'Lampu besar',
+            'Lampu kecil',
+            'Lampu Rem',
+            'Lampu belakang',
+            'Lampu Mundur',
+            'Lampu Kabut',
+            'Asuransi kendaraan',
+        ];
+
+        foreach ($components as $component) {
+            Component::create(['nama_komponen' => $component]);
+        }
     }
 }
